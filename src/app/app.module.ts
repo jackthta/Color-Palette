783f034faaaeb157573a899fbdd5551a5e5c1225
jackthta <jack.th.ta@gmail.com>
2021-01-19 11:02:49 -0800

    Added + color category button to Home component. No functionality yet.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header/header.component';
+import { HeaderComponent } from './home/header/header.component';
 import { HomeComponent } from './home/home/home.component';
 import { FooterComponent } from './home/footer/footer.component';
 import { PageNotFoundComponent } from './auth/page-not-found/page-not-found.component';
